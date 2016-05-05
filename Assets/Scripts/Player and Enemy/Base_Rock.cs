93f41b1ad77b6bbc9ac8991332483e04e899c7be
IHaveLiverCancer <jasperbardsley@yahoo.com.au>
2016-05-05 16:48:59 +1000

    Third Wave of Assets Battle Scene Creation of Rock Classes and Default Variables

---
+++ Assets/Scripts/Player and Enemy/Base_Rock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Base_Rock : Object {
+
+    protected float attack, defense, speed, luck, health;
+    protected float finalDamageOutput;
+
+    protected string rockName;
+    protected string rockDescription;
+
+    protected string SpriteID;
+
+
+    public float Defense{get { return defense; }}
+    public float Speed { get { return speed; } }
+    public float Damage { get { return finalDamageOutput; } }
+
+    public abstract void Combat();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
