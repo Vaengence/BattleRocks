f6bf62809e0e0b1287c856ae3a81da3b64d41989
Vaengence <michael.richardson@students.aie.edu.au>
2016-05-08 02:32:13 +0930

    My feeble attempts to add stuff

--- Assets/Scripts/Store/ItemButton.cs
+++ Assets/Scripts/Store/ItemButton.cs
@@ -5,21 +5,14 @@ using UnityEngine.UI;
 public class ItemButton : MonoBehaviour {
 
     Image ItemImage;
-    Text ItemText;
 
-    public void Init(Sprite InventoryItem, string InventoryName)
+    public void Init(Sprite InventoryItem, string StoreItemImage)
     {
         if(ItemImage == null)
         {
             ItemImage = GetComponent<Image>();
         }
 
-        if (ItemText == null)
-        {
-            ItemText = GetComponentInChildren<Text>();
-        }
-
         ItemImage.sprite = InventoryItem;
-        ItemText.text = InventoryName;
     }
 }
--- Assets/Scripts/Store/StoreManager.cs
+++ Assets/Scripts/Store/StoreManager.cs
@@ -1,29 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System;
 
 public class StoreManager : MonoBehaviour {
 
     public ItemButton StoreIconPrefab;
 
-    public Sprite[] ItemSprites;
-    public string[] ItemNames;
+    public Sprite[] ButtonSprites;
 
-    public Transform StorePanel;
+    public struct StoreObjectsStruct
+    {
+        public int StoreItemIndex;
+        public string StoreItemImage;
+    };
+
+    public StoreObjectsStruct[] StoreObjects;
 
     // Use this for initialization
     void Start ()
     {
-        for (int i = 0; i < ItemNames.Length; i++)
+        ButtonSprites = Resources.LoadAll<Sprite>("Sprites\\Game\\Character Profile\\Custom Item Icons\\");
+        StoreObjects = new StoreObjectsStruct[ButtonSprites.Length];
+
+        for (int i = 0; i < ButtonSprites.Length; i++)
         {
             GameObject Temp = Instantiate(StoreIconPrefab.gameObject, transform.position, transform.rotation) as GameObject;
             Temp.transform.SetParent(transform);
 
             ItemButton IbTemp = Temp.GetComponent<ItemButton>();
-            IbTemp.Init(ItemSprites[i], ItemNames[i]);
+            IbTemp.Init(ButtonSprites[i], StoreObjects[i].StoreItemImage);
         }
+    }
 
-	}
-	
 	// Update is called once per frame
 	void Update () {
 	
