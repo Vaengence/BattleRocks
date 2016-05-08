016628ff6eb6bf2e10c7ab3ff743a5a4f2c1dd06
MrStojsic <michael.stojsic@students.aie.edu.au>
2016-05-08 13:33:19 +1000

    Re-added the Main Menu and Options Menu functionality back. Finished the GameManager to use PlayerPrefs to store and save all save data.

--- Assets/Scripts/Menu/_GameManager.cs
+++ Assets/Scripts/Menu/_GameManager.cs
@@ -1,41 +1,54 @@
 ﻿// Author: Michael Stojsic 2016.
-// NOTE: Attach this script tot the 'Game Manager' Object and remember to set execution order,
-//       (edit > Project Settings > Script Execution Order > Set GameManger to -100)
-//       so GameManager runs a little earlier than all other scripts in the game, 
-//       otherwise values taken from _GameManager.instance may not be initialised yet and result in incorrect values used.
-// - This script will manage any variables used between multiple scenes like scores, timers and current level loaded.
+// NOTE: - Attach this script to the 'Game Manager' in the first loaded scene, it will then live throughout each scene from there.
+//       - For debugging you can place a Gamemanger in every scene but note any changes made to the non _instance scene GameManagers MUST be updated to the _instance one in the first scene,
+//         as the frist scenes GameManager will be the _instacne when the player plays the game and will delete any non _instance GameManagers if comes across.
+//       - remember to set execution order, go to unity (edit -> Project Settings -> Script Execution Order -> Set GameManger to -100)!!!
+//         so GameManager runs a little earlier than all other scripts in the game, 
+//         otherwise values taken from _GameManager.instance may not be initialised yet and result in incorrect values used.
+//       - This script will manage any variables used between multiple scenes like lives, money, playerNames.
 using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-
 public class _GameManager : MonoBehaviour
 {
-    // ---------------------------- VARIABLES ----------------------------
-    private static _GameManager _instance;               // - Internal GameManager reference.
-    public static _GameManager instance                  // - External GameManager reference for all other scenes.
+    // ---------------------------- CREATE AS INSTANCE ---------------------------- \\
+    // - This section checks if a current _instacne of GameManager exists, if it does it returns the _instance,
+    //   otherwise it creates a new GameManger in the current scene called "GameManager" 
+    //   sets it to the _instance of Gamamanger and adds this _GameManager script to it, 
+    //   essentailly making it the _instance.
+    private static _GameManager _instance;                                  // - Internal GameManager reference.
+    public static _GameManager instance                                     // - External GameManager reference for all other scenes.
     #region GameManager instance.       
     {
         get
         {
-            if(_instance != null)                       // - If _instance not null.
+            if(_instance != null)                                           // - If _instance not null.
             {
-                return _instance;                       // - Return current _instance.
+                return _instance;                                           // - Return current _instance.
             }
-            else                                        // - Else if _instance not null.
+            else                                                            // - Else (if _instance not null).
             {
-                GameObject gameManager = new GameObject("GameManager");     // - Set gameMnager as a new GO called GameManager.
-                _instance = gameManager.AddComponent<_GameManager>();        // - Add GameManager script to that new GO _instance.
-                return _instance;                                           // - Return than new instance of GameManager as _instance.
+                GameObject gameManager = new GameObject("GameManager");     // - Set gameMnager as a new GameObject called GameManager.
+                _instance = gameManager.AddComponent<_GameManager>();       // - Add GameManager script to that new GameObject _instance.
+                return _instance;                                           // - Return that new instance of GameManager as _instance.
             }
         }
     }
-    #endregion
-    // GLOBAL VARIABLES.
+    #endregion  
+    // ---------------------------- VARIABLES ---------------------------- \\
+    // - THis is where we store the Variables that are needed game wide, 
+    //   being in the GameManager allows them to live for the entire life of the current play session.
+    // Player Variables.
+    public string playerName;
+    public int currentLives;
+    public int cashCurrency;
+    public string lastLevelPlayed;                                // - Stores last level played.  
 
+    // Audio Variables. 
     public bool soundOn = true;
     public bool musicOn = true;
 
-    // LEVEL VARIABLES. 
+    // Unlocked Levels Variables 
     public int[] unlockedLevels = new int[]                     // - Stores which levels are currently unlocked. 1 = unlocked, 0 = locked. used ints rathen than bool as easier to save and load imo.
     {
         0,  // [0] Currently unused to keep code readable and understandable.
@@ -45,13 +58,11 @@ public class _GameManager : MonoBehaviour
         0,  // [4] level 4
         0,  // [5] level 5
 
-        // NOTE: ADD NEW ELEMENTS AS WE ADD LEVELS.
+        // NOTE: ADD NEW INDEXES AS WE ADD LEVELS.
     };
-    public int currentLevel = 1;                                // - Stores current level played.
-    public bool levelFinished = false;                          // - Stores whether player has finished current level or not.
-    public string lastLevelPlayed;                              // - Stores the last level played, set each time a level is loaded and is then used by the Continue button in the Main menu
+        
     // ---------------------------- START ----------------------------
-    void Start () 
+    void Awake () 
     {
         // Set _instance as this script.
         if(_instance != this)                                   // - If _instance isnt this GameManager.
@@ -82,23 +93,31 @@ public class _GameManager : MonoBehaviour
     // - Saves all important variables to playerPrefs.  Not accesable directly by outside scripts.
     void SaveGame()
     {
-        // Saves Unlocked Levels.
+        // Save Current Player Variables.
+        PlayerPrefs.SetString("Player Name", playerName);                                           // - Set playerPrefs Player Name to playerName as a string.
+        PlayerPrefs.SetInt("CurrentLives", currentLives);                                           // - Set playerPrefs Current Lives to currentLives as an int.
+        PlayerPrefs.SetInt("Cash Currency", cashCurrency);                                          // - Set playerPrefs Cash Currency to cashCurrency as an int.
+        PlayerPrefs.SetString("Last Level Played", lastLevelPlayed);                                // - Set playerPrefs Last Level Played to lastLevelplayed as a string.
+        // Save Current Unlocked Levels.
         for (int i = 0; i < unlockedLevels.Length; i++)                                             // - For loop through unlockedLevels.
         {
             PlayerPrefs.SetInt("Unlocked Levels" + i.ToString(), unlockedLevels[i]);                // - Set playerPrefs UnlockedLevel 'i' as unlockedLevels index 'i'.
         }
-        PlayerPrefs.SetString("Last Level Played", lastLevelPlayed);                                // - Set playerPrefs Last Level Played to lastLevelplayed as string .
     }
     // ---------------------------- LOAD GAME ----------------------------
     // - Loads all important variables from playerPrefs. Not accesable directly by outside scripts.
     void LoadGame()
     {
-        // Loads Unlocked Levels.
+        // Load Saved Player Variables.
+        playerName = PlayerPrefs.GetString("Player Name");                                          // - Set playerName to playerPrefs Player Name as a string.
+        currentLives = PlayerPrefs.GetInt("CurrentLives");                                          // - Set currentLives to playerPrefs Current Lives as an int.
+        cashCurrency = PlayerPrefs.GetInt("Cash Currency");                                         // - Set cashCurrency to playerPrefs Cash Currency as an int.
+        lastLevelPlayed = PlayerPrefs.GetString("Last Level Played");                               // - Set lastLevelplayed to playerPrefs Last Level Played as a string.
+        // Load Saved Unlocked Levels.
         for (int i = 0; i < unlockedLevels.Length; i++)                                             // - For loop through unlockedLevels.
         {
             unlockedLevels[i] = PlayerPrefs.GetInt("Unlocked Levels" + i.ToString());               // - Set unlockedLevels 'i' as playerPrefs Unlocked Levels index 'i'.
         }
-        lastLevelPlayed = PlayerPrefs.GetString("Last Level Played");                               // - Set lastLevelplayed to playerPrefs Last Level Played as string.
     }
     // ---------------------------- DELETE SAVE DATA ----------------------------
     // - Deletes all important variables from playerPrefs and sets the variables values to defaults 
@@ -106,15 +125,18 @@ public class _GameManager : MonoBehaviour
     public void DeleteSaveData()
     {
         PlayerPrefs.DeleteAll();                                    // - Deletes all playerPrefs.
+        playerName = " ";                                           // - Set playerName to " " so its empty as its a new game.
+        currentLives = 3;                                           // - Set currentLives to 3 as its a new game.
+        cashCurrency = 0;                                           // - Set cashCurrency to 0 as its a new game.
+        lastLevelPlayed = " "; //"first levels name here";          // - Set lastlevelPlayed to 1st level in the game so new game button runs correct level on new game.
         for(int i = 0; i < unlockedLevels.Length; i++)              // - For loop through unlockedLevels.
         {
             unlockedLevels[i]=0;                                    // - Reset each index of unlocked levels to 0 to re-lock them all.
         }      
         unlockedLevels[1] = 1;                                      // - Set index 1 of unlockedlevels to 1, so first level of game is unlocked when we clear our save.
 
-        // lastLevelPlayed = "first level";                          // - Set lastlevelPlayed to 1st level in the game so new game button runs correct level on new game.
         SaveGame();                                                 // - Call SaveGame to save new values as the saved values.
-        SceneManager.LoadScene("MenuMain");                         // - Load up Main_Menu scene.
+        SceneManager.LoadScene("MenuMain");                         // - Load up MainMenu scene.
     }   
     // ---------------------------- RESET GAME ----------------------------
     // - This will be called when the level is reset to reset all the values
@@ -122,13 +144,5 @@ public class _GameManager : MonoBehaviour
     public void ResetGame()
     {
         // NOTE: ADD - add any variable that need to be reset at end of level
-        levelFinished = false;              // - Set levelFinished to true to tell all scripts the current level was finished.
-    }
-    // ---------------------------- START GAME LEVEL ----------------------------
-    public void StartGameLevel()
-    // - This is called at the start of each level - 
-    // NOTE: This can be merged with RESET GAME if nt needed
-    {
-        levelFinished = false;              // - Set LevelFinished to false as level has just started.
     }
 }
